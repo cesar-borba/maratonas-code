26472ef0adf470e6a35f719cfca7bbf1862cccc8
César Borba <cesarmartins350@gmail.com>
2025-05-02 20:22:43 -0300

    Resolução problema C Interfatecs 2016 1fase

---
+++ provas/interfatecs_2016_1fase/problema_c.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define print(x) cout << x << "\n"
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+int main() { _
+
+    string seq1, seq2, seq3;
+    int len, numCadeias = 0;
+
+    cin >> seq1 >> seq2 >> seq3 >> len;
+
+    for(int i=0; i<=seq1.length()-len; i++) {
+        string cadeia = seq1.substr(i,len);
+        if (seq2.find(cadeia) != string::npos && seq3.find(cadeia) != string::npos) numCadeias++;
+    }
+
+    print(numCadeias);
+
+    return 0;
+}
